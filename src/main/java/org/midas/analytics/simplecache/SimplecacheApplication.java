c5cac02fd16799293f2ce7d2d988ba3a27b6f8b4
Arijit Nath <arijitnath92@gmail.com>
2020-07-21 18:37:13 +0530

    First Merge Cache Manager

---
+++ src/main/java/org/midas/analytics/simplecache/SimplecacheApplication.java
@@ -0,0 +1,13 @@
+package org.midas.analytics.simplecache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplecacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplecacheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/midas/analytics/simplecache/SimplecacheApplicationTests.java
@@ -0,0 +1,13 @@
+package org.midas.analytics.simplecache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplecacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
